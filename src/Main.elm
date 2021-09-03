b313784cc48efce651f5f1bcccf54d24982cf8da
Cedd Burge <ceddlyburge@gmail.com>
2021-09-03 10:10:11 +0100

    Upgrade to Elm 0.19

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,53 @@
-import Html
+module Main exposing (..)
 
-main : Html.Html msg
-main = Html.text "Hello World"
+import Browser
+import Html exposing (Html, text, div, h1, img)
+import Html.Attributes exposing (src)
+
+
+---- MODEL ----
+
+
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, Cmd.none )
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view model =
+    h1 [] [ text "Hello world! (Not progressive)" ]
+
+
+
+---- PROGRAM ----
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { view = view
+        , init = \_ -> init
+        , update = update
+        , subscriptions = always Sub.none
+        }
