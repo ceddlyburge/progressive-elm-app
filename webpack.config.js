08b5bc6750f56e38bc005c417ffd147a5f12c341
Cedd Burge <ceddlyburge@users.noreply.github.com>
2021-09-03 09:41:53 +0100

    Remove warn elm compiler option
    Which doesn't exist in Elm 0.19

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
       {
         test:    /\.elm$/,
         exclude: [/elm-stuff/, /node_modules/],
-        loader:  'elm-webpack-loader?verbose=true&warn=true',
+        loader:  'elm-webpack-loader?verbose=true',
       }
     ],
 
